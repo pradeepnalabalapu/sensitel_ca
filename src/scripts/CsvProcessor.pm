82abeb3dc61fa6f9021a6aab0a63b4291f0cb36b
Pradeep Nalabalapu <pradeep@clemetric.com>
2015-03-12 06:58:25 -0700

    fixes

--- src/scripts/CsvProcessor.pm
+++ src/scripts/CsvProcessor.pm
@@ -1,23 +1,29 @@
+our $DB_SW_NAME;
+our $STORAGE_SIZE;
+our $DB_SIZE;
 package CsvProcessor;
 use strict;
+use List::Util 'max';
 
 my $DEBUG_FLOW = 1;
 sub new {
 	my $class = shift;
 	my $filename = shift;
+	my $db_sw_name = shift;
 	my $keywordArray = shift;
 	my $debug = shift;
 	my $fd;
 	if ($debug & $DEBUG_FLOW) {
 		printf "Creating Processor object for file $filename\n";
 	}
-	open ($fd, $filename) or die "Can't read file $filename\n"; 
+	open ($fd, $filename) or die "ERROR : Can't read file $filename\n"; 
 
 	my $self = {
 		filename => $filename,
 		fd => $fd,
 		keywords => $keywordArray,
 		debug => $debug,
+		db_sw_name => $db_sw_name,
 	};
 	
 	bless $self, $class;
@@ -48,27 +54,66 @@ sub getHeader {
 		
 	for (my $i=0; $i < $len ; $i++){
 		my $key = $self->{keywords}[$i];
-		for (my $j=0; $j<$headerlen; $j++) {
-			#printf ("\t\tkey=$key headerToken=$headerTokens[$j]\n");
-			if ($headerTokens[$j] =~ m:$key:) {
-				$indexOfKey[$i] = $j;
-				printf ("indexOfKey[$key] = $j\n") if ($self->{debug} & $DEBUG_FLOW);
-				last;
+		printf ("key = $key\n") if ($self->{debug} & $DEBUG_FLOW);
+		if($key eq 'DB SW') {
+			$indexOfKey[$i] = -1;
+		} else {
+			for (my $j=0; $j<$headerlen; $j++) {
+				#printf ("\t\tkey=$key headerToken=$headerTokens[$j]\n");
+				if ($headerTokens[$j] =~ m:$key:) {
+					$indexOfKey[$i] = $j;
+					printf ("indexOfKey[$key] = $j\n") if ($self->{debug} & $DEBUG_FLOW);
+					last;
+				}
 			}
 		}
 	}
+
+	printf ("indexOfKey = @indexOfKey\n") if ($self->{debug} & $DEBUG_FLOW);
 	$self->{indexOfKey} = \@indexOfKey;
 }
 
 sub readData {
 	my $self = shift;
-	my $outArray = shift;
+	my $outArrayRef = shift;
 	my $line;
 	my $fd = $self->{fd};
+	my @tokens;
+	my @indexOfKeys = @{$self->{indexOfKey}};
 	while ($line = <$fd>) {
+		my @tokens = split(',', $line); 
+		my $storage_token= $tokens[$indexOfKeys[$STORAGE_SIZE]];
 
+		#printf ("pre-storage_token = $storage_token\n");
+
+		if($storage_token =~ m:(\d*\.\d*)\s*TB*:) {
+			my $gb = ($1+0)*1000; 
+			$storage_token =~ s:$&:$gb GB:;
+		}
+		#printf ("post-storage_token = $storage_token\n");
+
+		my @numbers = ($storage_token =~ m:\d*\s*GB:g);
+		#printf ("matching storage sizes = @numbers\n");
+		my $max_size = max(@numbers);
+		#printf ("max storage size $max_size\n");
+		$max_size =~ s:\s*GB::;
+		$tokens[$indexOfKeys[$STORAGE_SIZE]] = $max_size;
+		$tokens[$indexOfKeys[$DB_SIZE]] =~ s:\s*GB::;
+
+
+
+		my @info = ();
+		for (my $i=0; $i < @indexOfKeys; $i++) {
+			my $value;
+			if($i == $DB_SW_NAME) {
+				$value = $self->{db_sw_name};
+			} else {
+				$value = $tokens[$indexOfKeys[$i]];
+			}
+			push (@info, $value);
+		}
+		push @$outArrayRef, \@info;
 	}
-
 
 }
 
--- src/scripts/process_csv_files.pl
+++ src/scripts/process_csv_files.pl
@@ -4,48 +4,156 @@ use strict;
 use lib 'src/scripts';
 use CsvProcessor;
 
-my $DEBUG_FLOW = 1;
+my $outJSONFile = "out.json";
+
+my $DEBUG_FLOW = 0;
 my $DEBUG=$DEBUG_FLOW;
 
 
-my $SNO = 0;
-my $DATACENTER = 1;
-my $DB_SW_NAME = 2;
-my $DB_TYPE = 3;
-my $DB_VERSION = 4;
-my $STORAGE_SIZE = 5;
-my $DB_SIZE = 6;
-my $PRODUCT = 7;
+our $SNO = 0;
+our $DATACENTER = 1;
+our $DB_SW_NAME = 2;
+our $DB_TYPE = 3;
+our $DB_VERSION = 4;
+our $STORAGE_SIZE = 5;
+our $DB_SIZE = 6;
+our $PRODUCT = 7;
+
+my @keys = ();
+$keys[$SNO] = 'SerialNo';
+$keys[$DATACENTER] = 'dataCenter';
+$keys[$DB_SW_NAME] = 'dBProg';
+$keys[$DB_TYPE] = 'dBType';
+$keys[$DB_VERSION] = 'dBVersion';
+$keys[$STORAGE_SIZE] = 'storage';
+$keys[$DB_SIZE] = 'dBSize';
+$keys[$PRODUCT] = 'product';
+
 
 #mssql csv header - S.No,Data Center,Server Name,Database Type,Database Version,Storage Sizes,Database Sizes(GB),Product
 #oracle csv header - Data Center,Server Name,Database Type,Database Version,Storage Sizes,Database Sizes,Product,,,,,
 #mysql csv header - S.No,DB ServerName,Data Center,Product,Prod/Non Prod/ slave ,Mysql version ,Nimsoft Monitoring,Enerprise monitoring,DB Size,OS Size,Active / in active ,comments
 
-
 #keyword order SNO,DATACENTER,DB_SW_NAME,DB_TYPE, DB_VERSION, STORAGE_SIZE, DB_SIZE, PRODUCT
 my %keyword = (
-	'mysql' => ['S.No', 'Data Center',,'Prod/Non Prod/ slave','Mysql version','OS Size','DB Size', 'Product' ],
-	'mssql' => ['S.No', 'Data Center',,'Database Type','Database Version','Storage Sizes','Database Sizes','Product'],
-	'oracle' => ['Server Name', 'Data Center',,'Database Type','Database Version','Storage Sizes','Database Sizes', 'Product' ]
+	'mysql' => ['S.No', 'Data Center','DB SW','Prod/Non Prod/ slave','Mysql version','OS Size','DB Size', 'Product' ],
+	'mssql' => ['S.No', 'Data Center','DB SW','Database Type','Database Version','Storage Sizes','Database Sizes','Product'],
+	'oracle' => ['Server Name', 'Data Center','DB SW','Database Type','Database Version','Storage Sizes','Database Sizes', 'Product' ]
 );
 
 
 
+my @out = ();
 
 
 	printf ("----------------\n") if ($DEBUG & $DEBUG_FLOW);
 
 foreach my $csvfile (glob("data/*.csv")) {
 	$csvfile =~ m:data/(\D*)\d*.csv:;
-	my $key_keyword = $1;
+	my $dbsw = $1;
 	if ($DEBUG & $DEBUG_FLOW) {
-		printf ("key for keyword hash is $key_keyword\n");
+		printf ("key for keyword hash is $dbsw\n");
 	}
-	my $processor = new CsvProcessor($csvfile,$keyword{$key_keyword},$DEBUG);
+	my $processor = new CsvProcessor($csvfile,$dbsw,$keyword{$dbsw},$DEBUG);
 	$processor->getHeader();
+	$processor->readData(\@out);
 
 	printf ("----------------\n") if ($DEBUG & $DEBUG_FLOW);
+}
 
+#printJSON(\@out);
+printJSONbyProduct(\@out);
 
+sub printJSON {
+	my $fdout;
+	open($fdout, ">".$outJSONFile) or die ("ERROR : Create file $outJSONFile failed\n");
+	my $arrayRef = shift;
+
+	printf $fdout (" data : [\n");
+
+	for(my $i=0; $i<@$arrayRef; $i++){
+		printf $fdout ("\t{ ");
+		for(my $j=0; $j < @{$arrayRef->[$i]}; $j++) {
+			if($j) {
+				printf $fdout ", ";
+			}
+			printf $fdout ("$keys[$j] : ");
+			my $value = $arrayRef->[$i][$j];
+			if ($value =~ /\s/ ) {
+				printf $fdout "\"$value\"";
+			} else {
+				printf $fdout $value;
+			}
+		}
+		printf $fdout ("}\n");
+	}
+
+	printf $fdout ("]\n");
+}
+
+#JSON : byProduct : { name : 'product', children :[
+#           { name: dbversion, size: num_servers, color: db_sw } ... ]
+#        }
+sub printJSONbyProduct {
+	my $fdout;
+	my $outJSONFile_byProductNum = 'src/html/byProdNumServers.json';
+	open($fdout, ">".$outJSONFile_byProductNum) or die ("ERROR : Create file $outJSONFile_byProductNum failed\n");
+	my $arrayRef = shift;
+
+	my %hash= ();
+
+    for (my $i=0; $i <@$arrayRef; $i++) {
+        my $product = product_filter($arrayRef->[$i][$PRODUCT]);
+
+        my $db_version = $arrayRef->[$i][$DB_VERSION];
+        my $db_sw_name = $arrayRef->[$i][$DB_SW_NAME];
+        if(! exists $hash{$product}) {
+            $hash{$product} = { };
+        } 
+				if (!exists $hash{$product}{$db_version}) {
+					$hash{$product}{$db_version} = { sw => $db_sw_name, count => 0}
+				}
+        $hash{$product}{$db_version}{count}++;
+    }
+	printf $fdout qq({\n "name" : "byProductNum", "children" : [\n);
+
+	my @prods = keys %hash;
+	foreach my $prod (@prods) {
+		printf $fdout qq(\t{ "name" : "$prod", "children" : [\n);
+		my @db_vers = keys $hash{$prod};
+		foreach my $db_ver (@db_vers) {
+			printf $fdout qq(\t\t{ "name" : "$db_ver", "size": $hash{$prod}{$db_ver}{count}, "db_sw" : "$hash{$prod}{$db_ver}{sw}"});
+			printf $fdout "%s\n", ($db_ver ne $db_vers[-1]) ? ',':'';
+		}
+		printf $fdout qq(\t\t]\n\t});
+		printf $fdout "%s\n", ($prod ne $prods[-1])?',':'';
+	}
+
+	printf $fdout qq( ]\n});
+
+	close($fdout);
 
 }
+
+
+sub product_filter {
+	my $prod = shift;
+	if($prod =~ m:\n$:) {
+		$prod =~ s:$&::;
+	}
+	
+	if ($prod =~ m:clarity:i) {
+		$prod = 'clarity';
+	} elsif ($prod =~ m:csm:i){
+		$prod = 'csm';
+	} elsif ($prod =~ m:N/A:) {
+		$prod = 'unused';
+	} elsif ($prod =~ m:This server hosts no customers:){
+		$prod = 'unused';
+	} elsif ($prod =~ m:^\s*$:) {
+		$prod = 'unused';
+	}
+
+	return $prod;
+}
+
